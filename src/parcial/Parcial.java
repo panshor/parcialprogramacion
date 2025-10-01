07f84778ac0527c97bd54614e145cb017b6f5647
juanj <juanj@pansho>
2025-10-01 18:49:34 -0500

    parcial

---
+++ src/parcial/Parcial.java
@@ -0,0 +1,16 @@
+package parcial;
+
+/**
+ *
+ * @author juanj
+ */
+public class Parcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
